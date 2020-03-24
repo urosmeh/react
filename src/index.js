84414b4a5f034ea17e25b5ced8a9617d08001c4d
urosmeh <uros.meh@gmail.com>
2020-03-24 14:51:58 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function MyApp(){
+  return(
+  <ul>
+    <li>Toast</li>
+    <li>Jajca</li>
+    <li>Mleko</li>  
+  </ul>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MyInfo(){
+  return(
+    <div>
+      <h1>Uroš Meh</h1>
+      <p>
+        I like to play football and do Insanity Workout.
+      </p>
+      <ol>
+        <li>Paris</li>
+        <li>Barcelona</li>
+        <li>London</li>
+      </ol>
+    </div>
+  )
+}
+
+ReactDOM.render(<MyInfo/>, document.getElementById("root"));
